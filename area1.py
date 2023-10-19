7d188458fe14d2487c7d66fd63b02fc49c60bbcd
MIHIRGHADSHI <139873406+MIHIRGHADSHI@users.noreply.github.com>
2023-10-19 17:14:50 +0530

    Add files via upload

---
+++ area1.py
@@ -0,0 +1,6 @@
+import func1 as f1
+# WAP to find the area of circle when given radius
+r=int(input("Enter Radius : "))
+#call function
+ar=round(f1.area_circle(r),2)
+print("Area of circle : ",ar)
---
+++ func1.py
@@ -0,0 +1,8 @@
+def square(n): #n is local variable
+    return n*n  #or n**2
+
+def cube(n):
+    return n**3 #** Exponent operator
+
+def area_circle(r,pi=3.14): #default argument
+    return pi*r*r
