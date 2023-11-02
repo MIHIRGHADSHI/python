0cab3a42f1722ee259b073bf9022a220c9ee3199
MIHIRGHADSHI <139873406+MIHIRGHADSHI@users.noreply.github.com>
2023-11-02 16:36:48 +0530

    Add files via upload

---
+++ Oops.py
@@ -0,0 +1,21 @@
+class Person:
+    hands=1
+    legs=2
+    
+    def walking(self):
+        print("Hi.. I'm Human. I can walk")
+
+    def talking(self):
+        print("Hi.. I'm Human. I can talk")
+
+human1=Person()
+print(human1.hands)
+print(human1.legs)
+human1.walking()
+human1.talking()
+print(type(human1))
+
+
+
+
+              
